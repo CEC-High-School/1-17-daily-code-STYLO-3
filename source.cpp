92040dd45f8604182327bc451236bde2483bde52
STYLO-3 <35541285+STYLO-3@users.noreply.github.com>
2018-01-17 13:09:06 -0700

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hello girls what brings the boys to the yard" << endl << endl << endl << endl;
+
+
+
+
+	
+	cout << "memes bring the boys to the yard" << endl(10000); 
+
+
+
+
+
+
+
+
+
+ }
